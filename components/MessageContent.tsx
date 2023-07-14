e1c3e3dc08d1c136bdcad793b35e882ac84ccca1
Ngu Wei Xian <weixianwu54@gmail.com>
2023-07-15 01:02:48 +0800

    fix: responsive layout MessageContent textAlign

--- components/MessageContent.tsx
+++ components/MessageContent.tsx
@@ -27,7 +27,7 @@ export default function MessageContent({ message }: MessageContentProps) {
                     className="self-start w-[40px] h-[40px] rounded-full object-cover shadow-sm translate-y-[-16px]"
                 />
             )}
-            <p className={`text-sm sm:text-md max-w-[170px] sm:max-w-[300px] p-4 border-[1.5px] border-slate-300 rounded-xl ${message.role === "user" ? "rounded-tr-none" : "rounded-tl-none"}`}>
+            <p className={`text-sm ${message.role === "user" && 'text-right'} sm:text-start sm:text-md max-w-[170px] sm:max-w-[300px] p-4 border-[1.5px] border-slate-300 rounded-xl ${message.role === "user" ? "rounded-tr-none" : "rounded-tl-none"}`}>
                 {message.content}
             </p>
         </div>
